943f9b8460c5a202eb6a33fee2cc1f51f720072d
averywallis <averybwallis@gmail.com>
2019-04-12 16:58:34 -0400

    Update ece2799designreview2demo.ino
    added descriptions, comments, and documentation

--- ece2799designreview2demo/ece2799designreview2demo.ino
+++ ece2799designreview2demo/ece2799designreview2demo.ino
@@ -1,7 +1,44 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <FastLED.h>
+/*
+ * Code used for design review #2 demonstration
+ * 
+ * Reads thermistor circuit output and display corresponding temperature
+ * 
+ * Shorted pot used to simulate thermistor
+ * 
+ * Wiring connections and circuit descriptions:
+ * 
+ * Display:
+ * VCC - 5V
+ * GND - GND
+ * SCL - A5
+ * SDA - A4
+ * 
+ * 
+ * LED:
+ * 220 resistor between data in pin and Arduino pin
+ * VCC - 5v
+ * GND - GND
+ * Din - D5
+ * 
+ * 'Thermistor' circuit:
+ * Acts as voltage divider circuit
+ * R1 = 10kOhms
+ * R2 = 'Thermistor' (really 10k shorted potentiometer to act as variable resistor)
+ * 
+ * VCC - 3.3V
+ * GND - GND
+ * Vout - A0
+ * 
+ * 
+ * 
+ */
 
+//appropriate libraries
+#include <Adafruit_GFX.h> //display library
+#include <Adafruit_SSD1306.h> //display library
+#include <FastLED.h> //addressable LED library
+
+//macros used for display, LED, thermistor calculations
 #define OLED_RESET 4
 #define LED_PIN 5
 #define NUM_LEDS 1
@@ -17,17 +54,17 @@
 #define IDEALTEMP 57.8
 
 
-Adafruit_SSD1306 display(OLED_RESET);
-CRGB leds[NUM_LEDS];
+Adafruit_SSD1306 display(OLED_RESET); //instance of display object
+CRGB leds[NUM_LEDS]; //instance of LED object
 
-uint16_t samples[NUMSAMPLES];
+uint16_t samples[NUMSAMPLES]; //array of samples
 
 
-void setup() {
+void setup(){
   // put your setup code here, to run once:
-  delay(1000); //boot up safely
-//  Serial.begin(9600);
-  analogReference(EXTERNAL);
+  delay(1000); //boot up safely (not entirely required, but here because why not)
+  Serial.begin(9600); //setup communications between Arduino and computer
+  analogReference(EXTERNAL); //setup reference for ADC as external (3.3V in this case)
 
   //setup LED
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip ); //add leds
@@ -64,8 +101,8 @@ void loop() {
      average += samples[i];
   }
   average /= NUMSAMPLES;
-  Serial.print("Average analog reading "); 
-  Serial.println(average);
+  Serial.print("Average analog reading "); //print serially to computer
+  Serial.println(average); //send to computer
   
   // convert the value to resistance
   average = 1023 / average - 1;
@@ -73,7 +110,7 @@ void loop() {
   Serial.print("Thermistor resistance "); 
   Serial.println(average);
 
-  //example temp reading code
+  //example temp reading code, using simplified Steinhart-hart equation
   float steinhart;
   steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
   steinhart = log(steinhart);                  // ln(R/Ro)
@@ -83,21 +120,23 @@ void loop() {
   steinhart -= 273.15;                         // convert to C
 
   //set LED to appropriate color
-  if(steinhart > IDEALTEMP + 10){
-    leds[0] = CRGB(255, 0, 0);
+  if(steinhart > IDEALTEMP + 10){ //if hotter than ideal + 10
+    leds[0] = CRGB(255, 0, 0); //set to red
   }
-  else if(steinhart < IDEALTEMP + 10 && steinhart > IDEALTEMP - 10){
-    leds[0] = CRGB(0, 255, 0);
+  else if(steinhart < IDEALTEMP + 10 && steinhart > IDEALTEMP - 10){ //if +/- 10 from ideal
+    leds[0] = CRGB(0, 255, 0); //set to green
   }
-  else if(steinhart < IDEALTEMP - 10){
-    leds[0] = CRGB(0, 0, 255);
+  else if(steinhart < IDEALTEMP - 10){ //if colder than ideal - 10
+    leds[0] = CRGB(0, 0, 255); //set to blue
   }
   FastLED.show();
 
   //print out temperatures, via serial and display
-//  Serial.print("Temperature "); 
-//  Serial.print(steinhart);
-//  Serial.println(" *C");
+  Serial.print("Temperature "); 
+  Serial.print(steinhart);
+  Serial.println(" *C");
+
+  //display info on OLED
   display.clearDisplay();
   display.setCursor(0,0);
   display.print("ECE2799 Demo");
